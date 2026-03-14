bb6d80f5435d76389a9111997b44a52dd2929b1b
C_Jenkins <gbengachris87@gmail.com>
2026-03-14 09:30:14 +0100

    Add category, date, and location filters to Filter component with icons

--- app/filter.jsx
+++ app/filter.jsx
@@ -1,13 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Education from "../assets/images/Education.svg";
+import Heart from "../assets/images/Heart.svg";
+import PaintIcon from "../assets/images/PaintIcon.svg";
 import ScreenInfo from "../components/screenInfo";
 import { COLORS } from "../constants/colors";
 import { FONTS } from "../constants/fonts";
-
 const ContentButton = ({ icon, text }) => (
-  <View style={{ flexDirection: "row" }}>
-    {icon && icon}{" "}
+  <TouchableOpacity style={styles.contentBtn}>
+    {icon && icon}
     <Text style={{ fontFamily: FONTS.regular, fontSize: 14 }}>{text}</Text>
-  </View>
+  </TouchableOpacity>
 );
 
 const Filter = () => {
@@ -16,10 +18,42 @@ const Filter = () => {
       <ScreenInfo ScreenTitle={"Filter Events"} />
       <View style={styles.safeview}>
         <Text style={styles.filterText}>Filter</Text>
+        {/* CATEGORY */}
         <View style={styles.section}>
           <Text style={styles.sectionHeader}>Category</Text>
           <View style={styles.sectionContents}>
             <ContentButton text={"All Categories"} />
+            <ContentButton
+              icon={<Heart color={"#448AFF"} />}
+              text={"Charity"}
+            />
+            <ContentButton icon={<Heart color={"#448AFF"} />} text={"Health"} />
+            <ContentButton
+              icon={<Education color={"#448AFF"} />}
+              text={"Education"}
+            />
+            <ContentButton
+              icon={<PaintIcon color={"#448AFF"} />}
+              text={"Youth Programs"}
+            />
+          </View>
+        </View>
+        {/* DATE */}
+        <View style={styles.section}>
+          <Text style={styles.sectionHeader}>Date</Text>
+          <View style={styles.sectionContents}>
+            <ContentButton text={"This Week"} />
+            <ContentButton text={"This Month"} />
+            <ContentButton text={"Next Month"} />
+          </View>
+        </View>
+        {/* LOCATION */}
+        <View style={styles.section}>
+          <Text style={styles.sectionHeader}>Location</Text>
+          <View style={styles.sectionContents}>
+            <ContentButton text={"Lagos"} />
+            <ContentButton text={"Nigeria"} />
+            <ContentButton text={"West Africa"} />
           </View>
         </View>
       </View>
@@ -30,7 +64,7 @@ const Filter = () => {
 const styles = StyleSheet.create({
   safeview: {
     padding: 20,
-    backgroundColor: COLORS.background,
+    backgroundColor: COLORS.white,
     flex: 1,
   },
 
@@ -43,6 +77,35 @@ const styles = StyleSheet.create({
 
   sectionHeader: { fontFamily: FONTS.regular, fontSize: 24, color: "#454545" },
 
-  sectionContents: { backgroundColor: "#9E9E9E17" },
+  sectionContents: {
+    backgroundColor: "rgba(158, 158, 158, 0.09)",
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    flexWrap: "wrap",
+    borderRadius: 8,
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+  },
+
+  contentBtn: {
+    flexDirection: "row",
+    backgroundColor: "#f1f1f1",
+    //   backgroundColor: "red",
+    padding: 10,
+    borderRadius: 8,
+    gap: 10,
+    alignItems: "center",
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    marginVertical: 10,
+    elevation: 5,
+    // justifyContent: "flex-end",
+  },
+
+  section: { marginVertical: 20 },
 });
 export default Filter;

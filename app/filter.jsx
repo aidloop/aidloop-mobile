5fa7ef4954a7d7430f65971ef6997207d10399d4
C_Jenkins <gbengachris87@gmail.com>
2026-03-14 08:46:41 +0100

    `Added ContentButton component and category filter section to Filter component`

--- app/filter.jsx
+++ app/filter.jsx
@@ -3,12 +3,25 @@ import ScreenInfo from "../components/screenInfo";
 import { COLORS } from "../constants/colors";
 import { FONTS } from "../constants/fonts";
 
+const ContentButton = ({ icon, text }) => (
+  <View style={{ flexDirection: "row" }}>
+    {icon && icon}{" "}
+    <Text style={{ fontFamily: FONTS.regular, fontSize: 14 }}>{text}</Text>
+  </View>
+);
+
 const Filter = () => {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <ScreenInfo ScreenTitle={"Filter Events"} />
       <View style={styles.safeview}>
         <Text style={styles.filterText}>Filter</Text>
+        <View style={styles.section}>
+          <Text style={styles.sectionHeader}>Category</Text>
+          <View style={styles.sectionContents}>
+            <ContentButton text={"All Categories"} />
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -27,5 +40,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: COLORS.primary,
   },
+
+  sectionHeader: { fontFamily: FONTS.regular, fontSize: 24, color: "#454545" },
+
+  sectionContents: { backgroundColor: "#9E9E9E17" },
 });
 export default Filter;

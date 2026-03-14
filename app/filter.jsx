0b8e85472068f4c8b62f233858b9efd76ca05541
C_Jenkins <gbengachris87@gmail.com>
2026-03-14 08:26:51 +0100

    `Added import of Text component, styles for safeview and filterText, and updated JSX structure`

--- app/filter.jsx
+++ app/filter.jsx
@@ -1,13 +1,31 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import ScreenInfo from "../components/screenInfo";
+import { COLORS } from "../constants/colors";
+import { FONTS } from "../constants/fonts";
 
 const Filter = () => {
   return (
     <View>
       <ScreenInfo ScreenTitle={"Filter Events"} />
-      <View></View>
+      <View style={styles.safeview}>
+        <Text style={styles.filterText}>Filter</Text>
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  safeview: {
+    padding: 20,
+    backgroundColor: COLORS.background,
+    flex: 1,
+  },
+
+  filterText: {
+    fontFamily: FONTS.semibold,
+    fontSize: 28,
+    textAlign: "center",
+    color: COLORS.primary,
+  },
+});
 export default Filter;

c4547b3d4e9564e9caa18e471c3ffb9f639b9b80
C_Jenkins <gbengachris87@gmail.com>
2026-03-23 01:29:27 +0100

    `Refactor screenInfo component: replace SafeAreaView with View, adjust styles`

--- components/screenInfo.jsx
+++ components/screenInfo.jsx
@@ -1,23 +1,23 @@
 import { router } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import Back from "../assets/images/Back.svg";
 import { COLORS } from "../constants/colors";
 import { FONTS } from "../constants/fonts";
 
 const ScreenInfo = ({ ScreenTitle, subtext, icon }) => {
   return (
-    <SafeAreaView style={styles.header}>
-      <TouchableOpacity onPress={() => router.back()}>
+    <View style={styles.header}>
+      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
         <Back color={COLORS.white} height={34} width={34} />
       </TouchableOpacity>
-      <View style={{ flex: 1, alignSelf: "center" }}>
+
+      <View style={styles.titleContainer}>
         <Text style={styles.titleText}>{ScreenTitle}</Text>
-        {subtext && subtext}
+        {subtext}
       </View>
 
-      {icon && icon}
-    </SafeAreaView>
+      <View style={styles.iconContainer}>{icon}</View>
+    </View>
   );
 };
 
@@ -25,19 +25,24 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     backgroundColor: COLORS.primary,
-    height: 105,
-    // justifyContent: "",
-    padding: 10,
-    alignItems: "flex-start",
+    paddingTop: 50,
+    paddingBottom: 15,
+    paddingHorizontal: 15,
+    alignItems: "center",
     justifyContent: "center",
   },
-  titleText: {
-    textAlign: "center",
+
+  titleContainer: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleText: {
     fontFamily: FONTS.semibold,
     color: COLORS.white,
-    fontSize: 24,
-    alignSelf: "center",
+    fontSize: 22,
+    textAlign: "center",
   },
 });
+
 export default ScreenInfo;
